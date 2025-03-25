f509c7585733c21f0421c387dc4d30971d48b994
Gerilosk9 <gerardlosquino@gmail.com>
2025-03-25 11:00:05 +0100

    Vista creada

---
+++ src/Vistas/VistaCrearTarea.java
@@ -0,0 +1,21 @@
+package Vistas;
+
+import javax.swing.*;
+
+public class VistaCrearTarea {
+    private JTextField titulo;
+    private JComboBox comboBox1;
+    private JComboBox comboBox2;
+    private JComboBox comboBox3;
+    private JTextField horaRevision;
+    private JTextField descrpcion;
+    private JLabel tituloLabel;
+    private JLabel dificultadLabel;
+    private JLabel calendarioLabel;
+    private JLabel estadoLabel;
+    private JLabel frecuenciaLabel;
+    private JLabel horaRevisionLabel;
+    private JLabel descripcionLabel;
+    private JPanel Panel1;
+    private JButton crearButton;
+}
