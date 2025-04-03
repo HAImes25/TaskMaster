275404df657d9053b1dfaee513cab0afd64e845e
HAImes25 <IM2024032@imesmaresme.com>
2025-04-03 13:27:20 +0200

    03/04 Vista Inicio Sesion Creada

---
+++ src/Vistas/VistaInicioSesion.java
@@ -0,0 +1,17 @@
+package Vistas;
+
+import javax.swing.*;
+
+public class VistaInicioSesion {
+    private JPanel panelInicioSesion;
+    private JLabel LabelTitulo;
+    private JTextField textFieldNombre;
+    private JTextField textFieldContraseña;
+    private JButton confirmarButton1;
+    private JButton inicoDeSesionButton;
+    private JTextField textFieldConfirmarContraseña;
+    private JTextField textFieldEmail;
+    private JButton registrarButton;
+    private JLabel labelConfirmarConstraseña;
+    private JLabel labelEmail;
+}
