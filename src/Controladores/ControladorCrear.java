b5dbccb71636bd66cbde96e2a1079ec2a59e30b6
ainadelarosa <delarosacamposaina@gmail.com>
2025-04-30 10:55:24 +0200

    30-4

--- src/Controladores/ControladorCrear.java
+++ src/Controladores/ControladorCrear.java
@@ -40,6 +40,43 @@ public class ControladorCrear extends JFrame{
             public void actionPerformed(ActionEvent e) {
                 System.out.println("crear");
 
+
+                //No funciona el problema esta en alguna cosa del modeloTareaHector
+                modeloTarea.setTitulo(vistaCrear.getTitul());
+                System.out.println("Titulo");
+                modeloTarea.setDificultad(vistaCrear.getComboBoxDificultad());
+                System.out.println("dificultad");
+                //modeloTarea.getEstado(vistaCrear.getComboBoxEstado());
+                modeloTarea.setFrecuencia(vistaCrear.getComboBoxFrecuencia());
+                System.out.println("frecuencia");
+
+
+
+                vistaAdministradorDeTareas.getListaTareasPorEmpezar().clear();
+                vistaAdministradorDeTareas.getListaTareasEnProceso().clear();
+                vistaAdministradorDeTareas.getListaTareasCompletada().clear();
+                vistaAdministradorDeTareas.getListaTareasVencida().clear();
+
+
+                for (int i = 0; i < listaTareas.size(); i++) {
+                    if (listaTareas.get(i).getEstado() == "Por empezar"){
+                        vistaAdministradorDeTareas.getListaTareasPorEmpezar().add(listaTareas.get(i));
+
+                    } else if (listaTareas.get(i).getEstado() == "En proceso") {
+                        vistaAdministradorDeTareas.getListaTareasEnProceso().add(listaTareas.get(i));
+
+                    }else if (listaTareas.get(i).getEstado() == "Completada"){
+                        vistaAdministradorDeTareas.getListaTareasCompletada().add(listaTareas.get(i));
+
+                    } else if (listaTareas.get(i).getEstado() == "Vencida") {
+                        vistaAdministradorDeTareas.getListaTareasVencida().add(listaTareas.get(i));
+                    }
+                }
+
+
+
+
+
                 //al clicar s'ha de guardar la tarea a la lista tareas
 
 
