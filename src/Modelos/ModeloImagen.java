314c0900e6d4f612d1654f5d193848de2c42faec
Gerilosk9 <gerardlosquino@gmail.com>
2025-05-20 10:55:52 +0200

    intento imagenes

--- src/Modelos/ModeloImagen.java
+++ src/Modelos/ModeloImagen.java
@@ -1,4 +1,38 @@
 package Modelos;
 
+import java.util.ArrayList;
+
 public class ModeloImagen {
+
+    private String nombre = "";
+    private String path = "";
+
+    public ModeloImagen(String nombre, String path, ArrayList<ModeloImagen> listaImagenes) {
+        this.nombre = nombre;
+        this.path = path;
+        agregarImgLista(listaImagenes);
+
+    }
+
+    public void agregarImgLista(ArrayList<ModeloImagen> listaImagenes){
+        listaImagenes.add(this);
+    }
+
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+
+
 }
+
+
